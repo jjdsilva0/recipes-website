4717d9c72dea36fec3db31d7c9996b46b35baeec
Joel D'Silva <jjdsilva@unc.edu>
2024-06-30 12:41:35 -0400

    added a registration post route

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,10 @@
 import express from 'express'; //serves as framework to create the API
-import cors from 'cors'; // library that allows the setup the rules for the communication btw the front and backend (API requests)
+import cors from 'cors'; // library that allows the setup of the rules for the communication btw the front and backend (API requests)
 import mongoose from 'mongoose'; // allows to write queries and such to the database (mongoDB)
 import dotenv from 'dotenv';
 import { resolve } from 'path';
+
+import {userRouter} from './routes/users.js'
 
 // Load environment variables from .env file located in the project root
 dotenv.config({ path: resolve('../.env') })
@@ -11,6 +13,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/auth", userRouter)
 
 mongoose.connect(process.env.MYACCOUNT_CONNECTION)
 
--- server/src/models/Users.js
+++ server/src/models/Users.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 // A Schema is an object that defines the structure of the data
 const UserSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
-    password: {type: String, required: true, unique: false}
-})
+    password: {type: String, required: true},
+});
 
-export const UserModel = mongoose.model("users", UserSchema)
+export const UserModel = mongoose.model("users", UserSchema);
