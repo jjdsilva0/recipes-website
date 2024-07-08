380651cd868c9661f223449a0c9bcf012c8e98b9
Joel D'Silva <jjdsilva@unc.edu>
2024-07-08 16:50:46 -0400

    created basic styling for navbar with icons that have hover animations and works with different types of screens and created other styling for App.js

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,12 +1,31 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faFeather,
+  faFloppyDisk,
+  faRightToBracket,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const Navbar = () => {
   return (
     <div className="navbar">
-      <Link to="/"> Home</Link>
-      <Link to="/create-recipe"> Create Recipe</Link>
-      <Link to="/saved-recipes"> Saved Recipes</Link>
-      <Link to="/auth"> Login/Register</Link>
+      <Link to="/" className="nav-link">
+        <FontAwesomeIcon icon={faHouse} className="icon" />
+        <span className="label">Home</span>
+      </Link>
+      <Link to="/create-recipe" className="nav-link">
+        <FontAwesomeIcon icon={faFeather} className="icon" />
+        <span className="label">Create Recipe</span>
+      </Link>
+      <Link to="/saved-recipes" className="nav-link">
+        <FontAwesomeIcon icon={faFloppyDisk} className="icon" />
+        <span className="label">Saved Recipes</span>
+      </Link>
+      <Link to="/auth" className="nav-link">
+        <FontAwesomeIcon icon={faRightToBracket} className="icon" />
+        <span className="label">Login/Register</span>
+      </Link>
     </div>
   );
 };
